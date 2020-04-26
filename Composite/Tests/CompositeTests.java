33727b8100984a290eb4580a6be36da1aec0f562
JackNarowski <jacknarowski@gmail.com>
2020-04-26 09:23:09 -0500

    Added CompositeTests.java

---
+++ Composite/Tests/CompositeTests.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+public class CompositeTests {
+    @Test
+    void TestStepCreation(){
+        Step step = new Step("Step", 10);
+        assertEquals("Step", step.name);
+        assertEquals(10, step.getTime());
+    }
+    @Test
+    void TestComposite(){
+        Step stepOne = new Step("one", 10);
+        Step stepTwo = new Step("two", 10);
+        Composite composite = new Composite("Composite");
+        assertEquals("Composite", composite.name);
+        assertEquals(0, composite.getTime());
+        composite.add(stepOne);
+        composite.add(stepTwo);
+        assertEquals(20, composite.getTime());
+        composite.remove(stepTwo);
+        assertEquals(10, composite.getTime());
+    }
+}
